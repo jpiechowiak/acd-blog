a8f981a4cda4c2a5b5c4138ee4395da39bd36c08
jpiechowiak <jerzy.piechowiak@gmail.com>
2014-01-24 13:14:33 +0100

    Added examples for ACD
    Added examples from ACD blog.

---
+++ formwizard/js/test.js
@@ -0,0 +1,79 @@
+﻿function InitDialogAndWizard(){
+    var oDialogNode = $("#dialog-example");
+    var oForm = oDialogNode.find("form");
+    if (!oDialogNode.data("dialog-created")) {
+        oForm.formwizard({
+            disableUIStyles: true
+        }); 
+        oForm.bind("step_shown", function(oEvent, oData) {
+			ManageStepButtons(oForm);
+			if("dialog-example-form-third" == oData.currentStep){
+				$('#spname').html($('#name').val());
+				$('#spsurname').html($('#surname').val());
+			}
+        });
+        oDialogNode.dialog({
+            width: 500,
+            height: 320,
+            title: "Kreator",
+			modal: true,
+            create: function(oEvent, oUi) {
+                $(this).data("dialog-created", true);
+            },			
+            beforeclose: function() {
+                $(this).find(':input').val('');
+            },
+            buttons: [{
+                id: "dialog-example-backbtn",
+                text: "Wstecz",
+                click: function() {
+                    oForm.formwizard("back");
+                },
+                css: { "width": "120px" }
+            },
+            {
+                id: "dialog-example-nextbtn",
+                text: "Dalej",
+                click: function() {
+                    if (oForm.formwizard("state").isLastStep) {
+                        alert("Koniec!");
+                    } else {
+                        oForm.formwizard("next");
+                    }
+                },
+                css: { "width": "120px" }
+            },
+            {
+                text: "Zamknij",
+                click: function() {
+                    $(this).dialog("close");
+                },
+                css: { "width": "120px" }
+			}]
+		});
+    }
+	ManageStepButtons(oForm);
+}
+
+function ManageStepButtons(oForm){
+	if (oForm.formwizard("state").isFirstStep) {
+		$("#dialog-example-backbtn").button("disable");
+	} else {
+		$("#dialog-example-backbtn").button("enable");
+	}
+	if (oForm.formwizard("state").isLastStep) {
+		$("#dialog-example-nextbtn").button("option", "label", "Zatwierdź");
+	} else {
+		$("#dialog-example-nextbtn").button("option", "label", "Dalej");
+	}   
+}
+
+$(document).ready(function() {
+	$("#btn-run")
+		.button()
+		.click(function(){
+			InitDialogAndWizard();
+			$("#dialog-example").dialog("open");
+		});
+		
+}); 
---
+++ greasemonkey/googleimage.user.js
@@ -0,0 +1,25 @@
+﻿// ==UserScript==
+// @name          Obrazy Google
+// @namespace     http://www.altcontroldelete.pl/userscripts/
+// @description	  Pozwala na ustawienie wymiarów obrazu za pomocą okienka prompt
+// @include       http://google.tld/*
+// @include       http://www.google.tld/*
+// ==/UserScript==
+
+var oImgSearchButton = document.getElementById('iszex_btn');
+if(null != oImgSearchButton){
+	var sSize = prompt("Wprowadź szerokość oraz wysokość obrazka rozdzielone " + 
+		"średnikiem, bądź wprowadź jedną wartość jeśli obrazek ma być kwadratem."
+	);
+	if((null != sSize) && ('' != sSize)){
+		anSizes = sSize.split(';');
+		if(anSizes.length > 1){
+			document.getElementById('iszw').value = anSizes[0];
+			document.getElementById('iszh').value = anSizes[1];
+		} else {
+			document.getElementById('iszw').value = anSizes[0];
+			document.getElementById('iszh').value = anSizes[0];		
+		}
+		oImgSearchButton.click();
+	}
+}
